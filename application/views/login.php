27bdc7b22a9bc30e131a2d0dc2b6400544725a2e
fadhilbagi <fadhil.bagi@gmail.com>
2021-01-06 21:40:31 +0700

    test commit

--- application/views/login.php
+++ application/views/login.php
@@ -16,7 +16,7 @@
   
   <!-- Custom styles for this template-->
   <link href="<?php echo base_url('css/sb-admin-2.min.css') ?>" rel="stylesheet">
-  
+  <!-- Custom styles for this template-->
 
 </head>
 
